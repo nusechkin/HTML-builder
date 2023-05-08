4d18d2e580f741ff7b00f6bbfaf9052fc3b21b43
nusechkin <komarova.ao@gmail.com>
2023-05-08 17:32:19 +0300

    Feat: implemented copying files without updates

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,5 +1,18 @@
-const path = require('path');
-const { readdir } = require('fs/promises');
+const { readdir, mkdir, copyFile } = require('fs/promises');
 const fs = require('fs/promises');
-const absPath = path.join(__dirname, 'files');
+const path = require('path');
 
+const absPath = path.join(__dirname, 'files');
+const newPath = path.join(__dirname, 'files-copy');
+
+(async () => {
+    const createDir = await mkdir(newPath, { recursive: true});
+    const files = await readdir(absPath, {withFileTypes: true});
+    for (const file of files) {
+        const filePath = path.join(absPath, file.name);
+        const newFilePath = path.join(newPath, file.name);
+        if (file.isFile()) {
+            const temp = await copyFile(filePath, newFilePath);
+        }
+    }
+})();
