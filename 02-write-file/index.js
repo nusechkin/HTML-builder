17601a236ae87031c8a154df56fa5207f19f5d42
nusechkin <komarova.ao@gmail.com>
2023-05-08 13:27:10 +0300

    Feat: write to file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,16 +3,28 @@ const path = require('path');
 const relativePath = path.join(__dirname, 'text.txt');
 fs.open(relativePath, 'w',  (err) => {
     if(err) throw err;
-    //console.log('Write something');
 });
-const readline = require('node:readline');
-const { stdin: input, stdout: output } = require('node:process');
 
-const rl = readline.createInterface({ input, output });
+const readline = require('readline');
+const { stdin, stdout } = require('process');
 
-rl.question('What do you think of Node.js? ', (answer) => {
-  // TODO: Log the answer in a database
-  console.log(`Thank you for your valuable feedback: ${answer}`);
+const rl = readline.createInterface({ input: stdin, output: stdout });
+const outputToFile = fs.createWriteStream(relativePath);
+stdout.write('Please, write something valuable\n');
 
+rl.on('line', (input) => {
+  if (input.includes('exit')) {
+    outputToFile.write('\n' + input.substring(0, input.indexOf('exit')));
+    bye();
+  }
+  else {
+    outputToFile.write('\n' + input);
+  }
+})
+
+rl.on('SIGINT', bye);
+
+function bye(){
+  stdout.write('Good luck');
   rl.close();
-});
+}
