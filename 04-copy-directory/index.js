82bf328c60666f2568828820dec78591959dfc45
nusechkin <komarova.ao@gmail.com>
2023-05-08 17:40:25 +0300

    Feat: updates added

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -7,6 +7,10 @@ const newPath = path.join(__dirname, 'files-copy');
 
 (async () => {
     const createDir = await mkdir(newPath, { recursive: true});
+    for (const file of await fs.readdir(newPath)) {
+        await fs.unlink(path.join(newPath, file));
+    }
+
     const files = await readdir(absPath, {withFileTypes: true});
     for (const file of files) {
         const filePath = path.join(absPath, file.name);
